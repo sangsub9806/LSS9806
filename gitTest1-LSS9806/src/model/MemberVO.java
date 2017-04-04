b25288cee059543e27e2209cb3f9983c446a05e0
KOSTA <KOSTA@KOSTA-HP>
2017-04-04 11:46:26 +0900

    MemberVO id 추가

--- gitTest1-LSS9806/src/model/MemberVO.java
+++ gitTest1-LSS9806/src/model/MemberVO.java
@@ -1,5 +1,18 @@
 package model;
 
 public class MemberVO {
+private String id;
 
+public MemberVO(String id) {
+	super();
+	this.id = id;
+}
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
 }
